aed0b730390396399d85064b57a51ad710566f47
ongtrandong2 <21521957@ms.uit.edu.vn>
2024-01-01 18:41:58 +0700

    update API

--- index.js
+++ index.js
@@ -102,7 +102,6 @@ const movieSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   name: String,
   email: String,
   password: String,
